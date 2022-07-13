e876db0c9cfe3af6c5372162d507c6736a4bece0
JorgeCab2711 <cab20009@uvg.edu.gt>
2022-07-13 00:50:16 -0600

    nice animation

--- Semana 1/gl.py
+++ Semana 1/gl.py
@@ -21,7 +21,7 @@ def color(r, g, b):
 
 
 class Render(object):
-    # Object Constructor
+    # Constructor del Objecto
     def __init__(self):
         self.viewPortX = 0
         self.viewPortY = 0
@@ -29,18 +29,14 @@ class Render(object):
         self.width = 0
         self.clearColor = color(0, 0, 0)
         self.currColor = color(1, 1, 1)
+        self.glViewPort(0, 0, self.width, self.height)
+
         self.glClear()
 
+    # Funcion para crear una ventana definiendo su alto y ancho
     def glCreateWindow(self, width, height):
         self.width = width
         self.height = height
-        self.glClear()
-
-    # Inside object initializer function
-    def glInit(self, width, height, r, g, b):
-        self.width = width
-        self.height = height
-        self.clearColor = color(r, g, b)
         self.glClear()
 
     # Function to create a BMP file
@@ -80,7 +76,13 @@ class Render(object):
         self.pixels = [[self.clearColor for y in range(self.height)]
                        for x in range(self.width)]
 
+    def glClearViewport(self, clr=None):
+        for i in range(self.viewPortX, self.viewPortX + self.viewPortWidth):
+            for j in range(self.viewPortY, self.viewPortY + self.viewPortHeight):
+                self.glPoint(i, j, clr)
+
     # Funcion para indicar el color con la que se llenara la matrix de pixeles
+
     def glClearColor(self, r, g, b):
         self.clearColor = color(r, b, g)
         self.glClear()
@@ -89,22 +91,28 @@ class Render(object):
     def glColor(self, r, g, b):
         self.currColor = color(r, g, b)
 
-    # Funcion para dibujar un punto en la matriz dew pixeles
+    # Funcion para dibujar un punto en la matriz dew pixeles en cualquier parte de la ventana
     def glPoint(self, x, y, clr=None):
-        if (0 <= x < self.width) and (0 < + y < self.height):
+        if (0 <= x < self.width) and (0 <= y < self.height):
             self.pixels[x][y] = clr or self.currColor
 
-    # Function to draw the viewPort square
-    def viewPortSquare(self):
-        for x in range(self.viewPortX, self.viewPortX + self.viewPortWidth):
-            for y in range(self.viewPortY, self.viewPortY + self.viewPortHeight):
-                self.pixels[x][y] = self.currColor
-
-    # Function to draw the viewPort
-
-    def glViewPort(self, x, y, width, height):
-        self.viewPortX = x
-        self.viewPortY = y
+    # Funcion para crear un viewport dentro de la matrix
+    def glViewPort(self, posX, posY, width, height):
+        self.viewPortX = posX
+        self.viewPortY = posY
         self.viewPortWidth = width
         self.viewPortHeight = height
-        self.viewPortSquare()
+
+    # Funcion para crear un punto dentro de los limites del punto con coordenadas normalizadas
+    def glViewPortPoint(self, normX, normY, clr=None):
+        # Verifica la posicion del punto llegando a 0.9999 por alguna razon cuando es 1 se sale del viewport
+        if normX > 0.9999 or normX < -0.9999 or normY > 0.9999 or normY < -0.9999:
+            print('Warning, coordinates out of viewport range.')
+        else:
+            x = (normX + 1) * (self.viewPortWidth/2) + self.viewPortX
+            y = (normY + 1) * (self.viewPortHeight/2) + self.viewPortY
+
+            x = int(x)
+            y = int(y)
+
+            self.glPoint(x, y, clr)
--- Semana 1/main.py
+++ Semana 1/main.py
@@ -1,15 +1,39 @@
-from gl import Render
-
-width = 500
-height = 500
+from gl import Render, color
+import time
+width = 512
+height = 512
 
 
 rend = Render()
 rend.glCreateWindow(width, height)
-# rend.glViewPort(7, 7, 500, 500)
+rend.glViewPort(int(width/4), int(height/4), int(width/2), int(height/2))
+rend.glClearColor(0.5, 0.5, 0.5)
+rend.glClear()
+rend.glClearViewport(color(0, 0, 0))
 
-for i in range(width):
-    rend.glPoint(i, i)
-
-
-rend.glFinish('output.bmp')
+# Ciclo que mientras la variable working sea true renderizara puntos dentro del viewport dependiendo el contador
+working = True
+Counter = 0
+while working:
+    # si el contador es cero, solo se dara color al punto de la esquina superior derecha
+    if (Counter == 0):
+        upperRight = rend.glViewPortPoint(0.9999, 0.9999, color(1, 0, 0))
+        center = rend.glViewPortPoint(0, 0, color(0, 0, 0))
+        lowerLeft = rend.glViewPortPoint(-0.9999, -0.9999, color(0, 0, 0))
+        Counter += 1
+    # si el contador es uno solo se le dara color al punto del centro
+    elif (Counter == 1):
+        upperRight = rend.glViewPortPoint(0.9999, 0.9999, color(0, 0, 0))
+        center = rend.glViewPortPoint(0, 0, color(1, 0, 0))
+        lowerLeft = rend.glViewPortPoint(-0.9999, -0.9999, color(0, 0, 0))
+        Counter += 1
+    # si el contador es dos solo se le dara color al punto de la esquina inferior izquierda
+    elif (Counter == 2):
+        upperRight = rend.glViewPortPoint(0.9999, 0.9999, color(0, 0, 0))
+        center = rend.glViewPortPoint(0, 0, color(0, 0, 0))
+        lowerLeft = rend.glViewPortPoint(-0.9999, -0.9999, color(1, 0, 0))
+        Counter += 1
+    elif (Counter == 3):
+        Counter = 0
+    time.sleep(0.25)
+    rend.glFinish('output.bmp')
