77d616fd5bbf4c9d01555a90237b155802993e76
Jorge Caballeros Pérez <cab20009@uvg.edu.gt>
2022-07-19 16:31:49 -0700

    restored

--- Semana2Lineas/main.py
+++ Semana2Lineas/main.py
@@ -1,9 +1,7 @@
 
-from tkinter import W
 from pip import main
 from gl import Render, color, V2
 import time
-
 
 width = 1000
 height = 500
@@ -11,56 +9,63 @@ height = 500
 
 rend = Render()
 rend.glCreateWindow(width, height)
+# rend.glViewPort(int(width/4), int(height/4), int(width/2), int(height/2))
+# rend.glClearColor(0.5, 0.5, 0.5)
+# rend.glClear()
+# rend.glClearViewport(color(0, 0, 0))
 
 
+def extra():
+    # Ciclo que mientras la variable working sea true renderizara puntos dentro del viewport dependiendo el contador
+    Counter = 0
+    while True:
+        # si el contador es cero, solo se dara color al punto de la esquina superior derecha
+        if (Counter == 0):
+            upperRight = rend.glViewPortPoint(0.9999, 0.9999, color(1, 0, 0))
+            center = rend.glViewPortPoint(0, 0, color(0, 0, 0))
+            lowerLeft = rend.glViewPortPoint(-0.9999, -0.9999, color(0, 0, 0))
+            Counter += 1
+        # si el contador es uno solo se le dara color al punto del centro
+        elif (Counter == 1):
+            upperRight = rend.glViewPortPoint(0.9999, 0.9999, color(0, 0, 0))
+            center = rend.glViewPortPoint(0, 0, color(1, 0, 0))
+            lowerLeft = rend.glViewPortPoint(-0.9999, -0.9999, color(0, 0, 0))
+            Counter += 1
+        # si el contador es dos solo se le dara color al punto de la esquina inferior izquierda
+        elif (Counter == 2):
+            upperRight = rend.glViewPortPoint(0.9999, 0.9999, color(0, 0, 0))
+            center = rend.glViewPortPoint(0, 0, color(0, 0, 0))
+            lowerLeft = rend.glViewPortPoint(-0.9999, -0.9999, color(1, 0, 0))
+            Counter += 1
+        elif (Counter == 3):
+            Counter = 0
+        time.sleep(0.25)
+        rend.glFinish('output.bmp')
 
+def requisito():
+    # Punto esquina superior derecha
+    rend.glViewPortPoint(0.9999, 0.9999, color(1, 0, 0))
+    # Punto centro
+    rend.glViewPortPoint(0, 0, color(1, 0, 0))
+    # punto esquina inferior izquierda
+    rend.glViewPortPoint(-0.9999, -0.9999, color(1, 0, 0))
 
-#mainMenu()
+    rend.glFinish('output.bmp')
+    print("Archivo BMP generado")
 
-# m =  1
-# b = int(height/2)
+def mainMenu():
+    print(
+        "Ingrese cualquiera de las siguientes opciones: \n[1] Requisitos basicos\n[2] Extra"
+    )
+    opcion = int(input(">> "))
 
-# for x in range(width):
-#     y = m * x + b
-#     y = int(y)
-#     rend.glPoint(x, y)
-
-# v0 = V2(width/2 , height/2)
-
-# dy = height / 100
-# dX = width / 100
-
-# x = 0
-# y = height
-
-# for i in range(100):
-#     rend.glLine(V2(x,0), V2(0,y))
-#     rend.glLine(V2(width,height), V2(width,y))
-#     rend.glLine(V2(x,height), V2(width,y))
-
-#     x += dX
-#     y -= dy
-
-def drawSquare(width, height):
-    # Square (horizontal, vertical)
-    v0 = V2(width*0.2 , height*0.1)
-    v1 = V2(width*0.2 ,height*0.2)
-    v2 = V2(width*0.3 ,height*0.2)
-    v3 = V2(width*0.3 ,height*0.1)
-    rend.glLine(v0,v1)
-    rend.glLine(v1,v2)
-    rend.glLine(v2,v3)
-    rend.glLine(v3,v0)
-
-def drawParall(width, height):
-    v0 = V2(width/2, height/2)
-    v1 = V2(width*0.6, height*0.6)
-    v2 = V2(width*0.4, height*0.6)
-    v3 = V2(width*0.3, height/2)
-    rend.glLine(v0,v1)  
-    rend.glLine(v1,v2)
-    rend.glLine(v2,v3)
-    rend.glLine(v3,v0)
+    if opcion == 1:
+        requisito()
+    elif opcion == 2:
+        print("Sequencia infinita iniciada, examinar archivo BMP")
+        extra()
+    else:
+        print('Error: Caracter desconocido dentro de las opciones.')
 
 def drawPenta(width, height):
     v0 = V2(width/8, height/2)
@@ -74,47 +79,34 @@ def drawPenta(width, height):
     rend.glLine(v3,v4)
     rend.glLine(v4,v0)
 
-def drawTriangle(width, height):
-    v0 = V2(width*0.9, height*0.5)
-    v1 = V2(width*0.7, height*0.5)
-    v2 = V2(width*0.9, height*0.7)
-    rend.glLine(v0,v1)  
-    rend.glLine(v1,v2)
-    rend.glLine(v2,v0)
+#mainMenu()
 
-def drawStar(width, height):
-    v0 = V2(width*0.9, height*0.5)
-    v1 = V2(width*0.85, height*0.7)
-    v2 = V2(width*0.8, height*0.5)
-    v3 = V2(width*0.9, height*0.65)
-    v4 = V2(width*0.8, height*0.65)
-    rend.glLine(v0,v1)  
-    rend.glLine(v1,v2)
-    rend.glLine(v2,v3)
-    rend.glLine(v3,v4)
-    rend.glLine(v4,v0)
+# m =  1
+# b = int(height/2)
 
+# for x in range(width):
+#     y = m * x + b
+#     y = int(y)
+#     rend.glPoint(x, y)
 
+v0 = V2(width/2 , height/2)
 
-# enProceso = True
-# while enProceso:
-#     drawPenta(width, height)
-#     rend.glFinish("output.bmp")
-#     time.sleep(1)
-#     drawTriangle(width, height)
-#     rend.glFinish("output.bmp")
-#     time.sleep(1)
-#     drawSquare(width, height)
-#     rend.glFinish("output.bmp")
-#     time.sleep(1)
-#     drawParall(width, height)
-#     rend.glFinish("output.bmp")
-#     time.sleep(1)
-#     drawStar(width, height)
-#     rend.glFinish("output.bmp")
-#     time.sleep(1)
-#     rend.glClearColor(0.0, 0.0, 0.0) 
-#     enProceso = False
+dy = height / 100
+dX = width / 100
+
+x = 0
+y = height
+
+for i in range(100):
+    rend.glLine(V2(x,0), V2(0,y))
+    rend.glLine(V2(width,height), V2(width,y))
+    rend.glLine(V2(x,height), V2(width,y))
+
+    x += dX
+    y -= dy
 
 
+
+
+rend.glFinish("output.bmp")
 
